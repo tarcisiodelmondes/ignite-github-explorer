b772393eb2e3672782ce3db3fb3d4d3174c516ca
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2021-11-02 08:24:03 -0300

    Primeiro component

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,26 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        <li>
+          <strong>unform</strong> <p>Form in React</p>
+          <a href="">Acessar repositório</a>
+        </li>
+
+        <li>
+          <strong>unform</strong> <p>Form in React</p>
+          <a href="">Acessar repositório</a>
+        </li>
+
+        <li>
+          <strong>unform</strong> <p>Form in React</p>
+          <a href="">Acessar repositório</a>
+        </li>
+      </ul>
+    </section>
+  );
+}
