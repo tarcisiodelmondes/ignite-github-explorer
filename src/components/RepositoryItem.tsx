331d702b9b76921edbf92ee8f53618c4984d9239
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2021-11-03 10:04:53 -0300

    Se aplicado esse commit, adicionara tipagem nos componentes

--- src/components/RepositoryItem.tsx
+++ src/components/RepositoryItem.tsx
@@ -1,6 +1,14 @@
+type RepositoryItemProps = {
+  repository: {
+    name: string;
+    description: string;
+    html_url: string;
+  };
+};
+
 export function RepositoryItem({
   repository: { name, description, html_url },
-}) {
+}: RepositoryItemProps) {
   return (
     <li>
       <strong>{name}</strong> <p>{description}</p>
--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,8 +3,14 @@ import { RepositoryItem } from "./RepositoryItem";
 import "../styles/repositories.scss";
 import { useEffect, useState } from "react";
 
+type Repositories = {
+  name: string;
+  description: string;
+  html_url: string;
+};
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repositories[]>([]);
 
   useEffect(() => {
     const url = "https://api.github.com/orgs/rocketseat/repos";
